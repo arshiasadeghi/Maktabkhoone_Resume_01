dd3e147fa5a31aafec786af5246a8e91410a2317
arshiasadeghi <59389097+arshiasadeghi@users.noreply.github.com>
2020-01-04 08:12:00 +0330

    Updates

---
+++ precache-manifest.3f3c2c474e379b255d0f9d0456fcb1b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82797d0e8d9aee0418e6a6cf376dff2",
+    "url": "/arshiasadeghi/resume01/index.html"
+  },
+  {
+    "revision": "5fff61b42914a9e0323f",
+    "url": "/arshiasadeghi/resume01/static/css/main.37284412.chunk.css"
+  },
+  {
+    "revision": "0c09702a6ac925350ebd",
+    "url": "/arshiasadeghi/resume01/static/js/2.9b31e940.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/arshiasadeghi/resume01/static/js/2.9b31e940.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fff61b42914a9e0323f",
+    "url": "/arshiasadeghi/resume01/static/js/main.8b21e280.chunk.js"
+  },
+  {
+    "revision": "ddc7b8a186213d383f31",
+    "url": "/arshiasadeghi/resume01/static/js/runtime-main.9cd14048.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/arshiasadeghi/resume01/static/media/logo.5d5d9eef.svg"
+  }
+]);
